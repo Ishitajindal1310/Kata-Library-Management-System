cc4395ae46fb394d3aac80da95b3ac00385454de
Ishita Jindal <ishijindal1310@gmail.com>
2024-12-26 18:14:57 +0530

    Test cases added

---
+++ LibraryManagementTest/LibraryManagementTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class LibraryManagementTest {
+    @Test
+    public void testAddBook() {
+        Library library = new Library();
+        Book book = new Book("Title", "Author", "12345");
+        library.addBook(book);
+        assertEquals(1, library.getBookCount());
+    }
+    @Test
+    public void testRemoveBook() {
+        Library library = new Library();
+        Book book = new Book("Title", "Author", "12345");
+        library.addBook(book);
+        library.removeBook("12345");
+        assertEquals(0, library.getBookCount());
+    }
+    @Test
+    public void testCheckOutBook() {
+        Library library = new Library();
+        Book book = new Book("Harry Potter", "J.K. Rowling", "12345");
+        library.addBook(book);
+        library.checkOutBook("12345");
+        assertFalse(book.isAvailable());
+    }
+    @Test
+    public void testReturnBook() {
+        Library library = new Library();
+        Book book = new Book("Harry Potter", "J.K. Rowling", "12345");
+        library.addBook(book);
+        library.checkOutBook("12345");
+        library.returnBook("12345");
+        assertTrue(book.isAvailable());
+    }
+
+
+}
